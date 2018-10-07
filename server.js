c32b1bf0c68dbb770217e2ebae26a4dd33539ff5
mithileshtipkari <mithilesh.tipkari@gmail.com>
2018-10-07 16:22:46 +0530

    servver.js chnaged pls see

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = require('./config/keys').mongoURI;
 
 //Connect to Mongo
 mongoose.connect(db)
-        .then(() => console.log('MongoDB connected'))
+        .then(() => console.log('MongoDB connected!'))
         .catch(err => console.log(err));
 
 //Use Routes
